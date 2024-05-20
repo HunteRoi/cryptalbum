35d6c1201121e5ea787344933ab796e54ad71c8a
Tinaël Devresse <9126633-hunteroi@users.noreply.gitlab.com>
2024-05-20 16:34:43 +0200

    refactor(ErrorFallback): divide into smaller parts (hook & components)

--- src/components/ErrorFallback.tsx
+++ src/components/ErrorFallback.tsx
@@ -9,48 +9,57 @@ type ErrorProps = {
 	reset: () => void;
 };
 
-type ErrorFallbackProps = {
-	isGlobalError?: boolean;
-};
 export default function ErrorFallback(
-	{ isGlobalError }: ErrorFallbackProps = { isGlobalError: false },
+	{ isGlobalError } = { isGlobalError: false },
 ) {
 	return function ErrorHandler({ error, reset }: ErrorProps) {
-		const { toast } = useToast();
+		useError(error);
 
-		useEffect(() => {
-			const message =
-				"message" in error
-					? error.message
-					: "An error occurred, and we failed to catch it before you did.";
-			toast({
-				title: "This is problematic!",
-				description: message,
-				variant: "destructive",
-			});
-		}, [toast, error]);
-
-		return isGlobalError ? (
-			<div>
-				<h1>Oops!</h1>
-				<p>Well. We fucked up... Sorry?</p>
-				<button type="button" onClick={reset}>
-					Try again, perhaps it fixed itself
-				</button>
-			</div>
-		) : (
-			<html lang="en">
-				<body>
-					<h1>Oops!</h1>
-					<p>
-						Too bad, you encountered an error that broke the hella great site we
-						made.
-					</p>
-					<button type="button" onClick={reset}>
-						Wanna try again?
-					</button>
-				</body>
-			</html>
-		);
+		return isGlobalError ? <ErrorDisplay reset={reset} /> : <GlobalErrorDisplay reset={reset} />;
 	};
+}
+
+function useError(error: ErrorProps['error']) {
+	const { toast } = useToast();
+
+	useEffect(() => {
+		const message =
+			"message" in error
+				? error.message
+				: "An error occurred, and we failed to catch it before you did.";
+		toast({
+			title: "This is problematic!",
+			description: message,
+			variant: "destructive",
+		});
+	}, [toast, error]);
+}
+
+function ErrorDisplay({ reset }: Pick<ErrorProps, 'reset'>) {
+	return (
+		<div>
+			<h1>Oops!</h1>
+			<p>Well. We fucked up... Sorry?</p>
+			<button type="button" onClick={reset}>
+				Try again, perhaps it fixed itself
+			</button>
+		</div>
+	);
+}
+
+function GlobalErrorDisplay({ reset }: Pick<ErrorProps, 'reset'>) {
+	return (
+		<html lang="en">
+			<body>
+				<h1>Oops!</h1>
+				<p>
+					Too bad, you encountered an error that broke the hella great site we
+					made.
+				</p>
+				<button type="button" onClick={reset}>
+					Wanna try again?
+				</button>
+			</body>
+		</html>
+	);
 }
